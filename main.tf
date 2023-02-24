b3fe8124e70c24bea7ab8dfaeba16f2bb6e0e176
Mahak Sharma <sharmamahak1359@gmail.com>
2023-02-25 01:02:02 +0530

    c

--- main.tf
+++ main.tf
@@ -9,14 +9,7 @@ terraform {
 provider "azurerm"{
   features  {}
 }
-//terraform{
-//backend "azurerm"{
-    //resource_group_name = "msiacrg"
-    //storage_account_name = "msiacstorageaccount"
-    //container_name = "msiaccontainer"
-    //key = "terraform.tfstate"
-//}
-//}
+
 
 resource "azurerm_resource_group" "resourcegroup" {
     name = "msiacrg"
