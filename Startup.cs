c356595f173c14abbc791b20ab779d9de045d8f6
Mahak Sharma <sharmamahak1359@gmail.com>
2023-02-24 20:39:21 +0530

    commit

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace weatherapi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "weatherapi v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
